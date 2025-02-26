fdc16a4cba442bcd5cd4a6a7e97c2d1696d0a153
Thanh Dat <38899985+datnt2996@users.noreply.github.com>
2025-02-26 14:35:19 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
-
+// eslint-disable-next-line
+const fs = require('fs');
 const myHeaders = new Headers();
 myHeaders.append('accept', '*/*');
 myHeaders.append('accept-language', 'en-US,en;q=0.9,vi;q=0.8');
@@ -22,29 +23,17 @@ myHeaders.append(
   'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36',
 );
 
-const requestOptions = {
+const API_OPTIONS = {
   method: 'GET',
   headers: myHeaders,
   redirect: 'follow',
 };
-const getPostDetail = async (id) => {
-  // eslint-disable-next-line
-  const response = await fetch(
-    `https://api-posts.khmer24.com/feed/${id}`,
-    requestOptions,
-  ).then((response) => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    return response.json();
-  });
-  return response?.data;
-};
+const FILE_NAME = 'ids_output.txt';
 const getPosts = async (offset) => {
   // eslint-disable-next-line
   const response = await fetch(
     `https://api-posts.khmer24.com/feed?fields=thumbnails,thumbnail,location,photos,user,store,renew_date,is_like,is_saved,category,link,object_highlight_specs,condition&functions=save,chat,like,apply_job,shipping,banner,highlight_ads%5Bobject_highlight_specs%5D&filter_version=4&meta=true&offset=${offset}&category=cars-and-vehicles&sortby&date&max_ad_price&min_ad_price&province&district&commune`,
-    requestOptions,
+    API_OPTIONS,
   )
     .then((response) => {
       if (!response.ok) {
@@ -64,23 +53,31 @@ const getPosts = async (offset) => {
 
 const TIME_ACTIONS = 500;
 const PAGE_SIZE = 30;
-const main = async (timeActions = TIME_ACTIONS) => {
+const getAllUrls = async (timeActions = TIME_ACTIONS) => {
+  const carIds = [];
+  // const urls = [];
   for (let i = 0; i < timeActions; i++) {
     const data = await getPosts(i * PAGE_SIZE);
-    const urls = [];
-    const result = []
     data.forEach((item) => {
-      if(item.type === 'post') {
-        urls.push(item.data.link);
-        result.push(item.data);
-      }
+      item?.type === 'post' && carIds.push(item?.data?.id);
     });
-    
-    console.log({ urls });
-    console.log({ data: data[0] });
-    console.log({ data: data[1] });
-    // writeToFile(data);
   }
+  await writeToTxtFile(FILE_NAME, carIds);
+  // await writeToTxtFile('output.txt', urls);
 };
 
-main(1);
+async function writeToTxtFile(name, data) {
+  try {
+    await fs.promises.writeFile(name, JSON.stringify(data, null, 2));
+    console.log('Data written to output successfully.');
+  } catch (error) {
+    console.error('Error writing to File:', error);
+  }
+}
+
+module.exports = {
+  getAllUrls,
+  API_OPTIONS,
+  FILE_NAME,
+};
+getAllUrls(2)
